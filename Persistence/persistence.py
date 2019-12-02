63394538f4777c169d2f9fcf032df511d8048e69
JeongHM <kjhm0607@gmail.com>
2019-12-02 20:57:04 +0900

    [solve] persistence

---
+++ Persistence/persistence.py
@@ -0,0 +1,39 @@
+# Write a function, persistence,
+# that takes in a positive parameter num and returns its multiplicative persistence,
+# which is the number of times you must multiply the digits in num until you reach a single digit.
+#
+# For example:
+#
+#  persistence(39) => 3  # Because 3*9 = 27, 2*7 = 14, 1*4=4
+#                        # and 4 has only one digit.
+#
+#  persistence(999) => 4 # Because 9*9*9 = 729, 7*2*9 = 126,
+#                        # 1*2*6 = 12, and finally 1*2 = 2.
+#
+#  persistence(4) => 0   # Because 4 is already a one-digit number.
+#  persistence(39) # returns 3, because 3*9=27, 2*7=14, 1*4=4
+#                  # and 4 has only one digit
+#
+#  persistence(999) # returns 4, because 9*9*9=729, 7*2*9=126,
+#                   # 1*2*6=12, and finally 1*2=2
+#
+#  persistence(4) # returns 0, because 4 is already a one-digit number
+
+
+
+def persistence(n):
+    count = 0
+    while n > 9:
+        count += 1
+        n = eval('*'.join([i for i in str(n)]))
+    return count
+
+
+# def persistence(n):
+#     count = 0
+#     while n > 9:
+#         count += 1
+#         result = reduce(lambda x, y: x * y, [int(i) for i in str(n)])
+#         n = result
+#         print(n)
+#     print('count:',count)
