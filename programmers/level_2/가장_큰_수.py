870a910d241920e0fbecb66d3a3450a82ac919db
JeongHM <kjhm0607@gmail.com>
2019-12-14 23:50:30 +0900

    [Solve] 가장 큰 수

--- programmers/level_2/가장_큰_수.py
+++ programmers/level_2/가장_큰_수.py
@@ -17,12 +17,11 @@
 # [6, 10, 2]	        6210
 # [3, 30, 34, 5, 9]	    9534330
 
-import itertools
-
-
 def solution(numbers):
     permutations = sorted(list(itertools.permutations(numbers)))
     return ''.join(sorted(permutations).pop())
 
 
+def solution2(numbers):
+    
 solution([3, 30, 34, 5, 9])
